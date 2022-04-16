8efa3e882d6523b313cc71430a9f013ed8286414
07alexander <07alexander@gmail.com>
2022-04-15 22:12:32 -0700

    user can now draw on the grid

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ function makeGrid(rows, cols){
     container.style.setProperty('--grid-cols', cols);
     for(let i=0;i<(rows*cols);i++){
         let cell = document.createElement("div");
-        cell.innerText=(i+1);
         container.appendChild(cell).className = "grid-item";
-        console.log("hi");
     };
 };
+
+    
 makeGrid(16,16);
+container.addEventListener("mouseover", function(event){
+        //event.target.style.color ="orange";
+        event.target.style.backgroundColor = "orange";
+    },false);
