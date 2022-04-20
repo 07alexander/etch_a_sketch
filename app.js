087f59e7b57387a03e4ccbdf2ddfbfe2a2fa47e7
07alexander <07alexander@gmail.com>
2022-04-19 20:31:18 -0700

    grid now contains squares and scales with browser

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container");
+
+
 function makeGrid(rows, cols){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -7,10 +9,21 @@ function makeGrid(rows, cols){
         container.appendChild(cell).className = "grid-item";
     };
 };
+function clearGrid(){
+    var board = container.querySelectorAll("grid-item");
+    for(let i=0;i<board.length; ++i){
+        board[i].style.backgroundColor ="";
+        console.log(board[i]);
+        console.log("hi");
+    }
+}
 
     
 makeGrid(16,16);
+
+
 container.addEventListener("mouseover", function(event){
-        //event.target.style.color ="orange";
         event.target.style.backgroundColor = "orange";
+        console.log(event);
     },false);
+
